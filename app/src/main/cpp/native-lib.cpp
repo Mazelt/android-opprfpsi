1ff2208ea1aa402e84330c92588b05725b8f4203
Mazelt <Pazeltma@hu-berlin.de>
2020-08-06 15:53:45 +0200

    Added logging, IT WORKS! (after updating original-opprf to newest libote)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,13 +2,56 @@
 #include <string>
 #include <cmath>
 #include <ENCRYPTO_utils/crypto/crypto.h>
-#include <ENCRYPTO_utils/connection.h>
-#include <ENCRYPTO_utils/socket.h>
+//#include <ENCRYPTO_utils/connection.h>
+//#include <ENCRYPTO_utils/socket.h>
 
 #include "abycore/aby/abyparty.h"
 #include "common/psi_analytics.h"
 #include "common/psi_analytics_context.h"
 
+#include <android/log.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static int pfd[2];
+static pthread_t loggingThread;
+static const char *LOG_TAG = "OPPRF COUT CERR";
+
+static void *loggingFunction(void*) {
+    ssize_t readSize;
+    char buf[128];
+
+    while((readSize = read(pfd[0], buf, sizeof buf - 1)) > 0) {
+        if(buf[readSize - 1] == '\n') {
+            --readSize;
+        }
+
+        buf[readSize] = 0;  // add null-terminator
+
+        __android_log_write(ANDROID_LOG_DEBUG, LOG_TAG, buf); // Set any log level you want
+    }
+
+    return 0;
+}
+
+static int runLoggingThread() { // run this function to redirect your output to android log
+    setvbuf(stdout, 0, _IOLBF, 0); // make stdout line-buffered
+    setvbuf(stderr, 0, _IONBF, 0); // make stderr unbuffered
+
+    /* create the pipe and redirect stdout and stderr */
+    pipe(pfd);
+    dup2(pfd[1], 1);
+    dup2(pfd[1], 2);
+
+    /* spawn the logging thread */
+    if(pthread_create(&loggingThread, 0, loggingFunction, 0) == -1) {
+        return -1;
+    }
+
+    pthread_detach(loggingThread);
+
+    return 0;
+}
 
 auto get_context() {
     ENCRYPTO::PsiAnalyticsContext context;
@@ -42,22 +85,23 @@ auto get_context() {
 }
 
 int run_opprf(ENCRYPTO::PsiAnalyticsContext &context){
-    std::unique_ptr<CSocket> sock = Connect(context.address.c_str(), context.port);
-    if (sock == NULL) {
-        return 1;
-    } else {
-        return 0;
-    }
-//    auto gen_bitlen = static_cast<std::size_t>(std::ceil(std::log2(context.neles))) + 3;
-//    auto inputs = ENCRYPTO::GeneratePseudoRandomElements(context.neles, gen_bitlen);
-//    ENCRYPTO::run_psi_analytics(inputs, context);
-//    return EXIT_SUCCESS;
+//    std::unique_ptr<CSocket> sock = Connect(context.address.c_str(), context.port);
+//    if (sock == NULL) {
+//        return 1;
+//    } else {
+//        return 0;
+//    }
+    auto gen_bitlen = static_cast<std::size_t>(std::ceil(std::log2(context.neles))) + 3;
+    auto inputs = ENCRYPTO::GeneratePseudoRandomElements(context.neles, gen_bitlen);
+    ENCRYPTO::run_psi_analytics(inputs, context);
+    return EXIT_SUCCESS;
 }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_opprf_1psi_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    runLoggingThread();
     auto context = get_context();
     int out = run_opprf(context);
     std::string hello = "Hello from C++ " + context.address + std::to_string(out);
